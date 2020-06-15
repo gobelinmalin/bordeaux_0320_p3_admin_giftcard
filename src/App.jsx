6804071c03304ff9d489734354688ee214b5a832
rachOS <g.chamekh@e-nautia.com>
2020-06-15 10:29:49 +0200

    add components folder and LeftMenu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 
+// import components
+import LeftMenu from './components/leftmenu/LeftMenu';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Hello React World</header>
-    </div>
-  );
+  return <LeftMenu />;
 }
 
 export default App;
---
+++ src/components/leftmenu/LeftMenu.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LeftMenu = () => {
+  return <>LEFT MENU</>;
+};
+
+export default LeftMenu;
