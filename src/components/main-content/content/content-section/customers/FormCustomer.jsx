1f6b9ee1921c40410d2279e8f9f865abc1003f18
rachOS <g.chamekh@e-nautia.com>
2020-06-22 22:24:14 +0200

    create form with .map() and basic table

--- src/components/main-content/content/content-section/customers/FormCustomer.jsx
+++ src/components/main-content/content/content-section/customers/FormCustomer.jsx
@@ -1,78 +1,33 @@
 // import lib
 import React from 'react';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import { Form, Col } from 'react-bootstrap';
 
 // import components
 
 // import style
 import '../content-section.css';
 
-const FormCustomer = () => {
+const FormCustomer = (props) => {
+  const { fields } = props;
+  const createFormClient = fields.map((field) => (
+    <Form.Group className="form-group">
+      <Form.Label>{field.label}</Form.Label>
+      <Form.Control type={field.type} size="sm" pattern={field.pattern} />
+    </Form.Group>
+  ));
   return (
-    <div>
-      <Form as={Col} className="form-client">
-        <Form.Group as={Row}>
-          <Form.Label column>identité du client</Form.Label>
-        </Form.Group>
-        <Form.Group controlId="formCreateClient">
-          <Form.Label column="sm">Nom</Form.Label>
-          <Form.Control type="string" size="sm" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label column="sm">Prénom</Form.Label>
-          <Form.Control type="string" size="sm" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label column="sm">Date de naissance</Form.Label>
-          <Form.Control type="date" size="sm" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label column="sm">Email</Form.Label>
-          <Form.Control type="email" size="sm" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label column="sm">Téléphone</Form.Label>
-          <Form.Control
-            type="tel"
-            size="sm"
-            pattern="[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}"
-          />
-          <small>format: 06-07-08-09-10</small>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label column="sm">Adresse postale</Form.Label>
-          <Form.Control type="string" size="sm" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label column="sm">Ville</Form.Label>
-          <Form.Control type="string" size="sm" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label column="sm">Code postal</Form.Label>
-          <Form.Control type="string" size="sm" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label column="sm">Pays</Form.Label>
-          <Form.Control type="string" size="sm" />
-        </Form.Group>
-        <Form.Group as={Col}>
-          <Form.Text as={Row} className="text-muted">
-            Veuillez remplir tous les champs requis avant de valider
-          </Form.Text>
-        </Form.Group>
-        <Form.Group as={Col}>
-          <Button as={Row} variant="primary" type="reset">
-            Annuler
-          </Button>
-        </Form.Group>
-        <Form.Group as={Col}>
-          <Button as={Row} variant="primary" type="submit">
-            Modifier le client
-          </Button>
-        </Form.Group>
-      </Form>
-    </div>
+    <Col className="col-form-label-sm">
+      <h3 id="">Identité du client</h3>
+      {createFormClient}
+    </Col>
   );
 };
 
+FormCustomer.defaultProps = {
+  fields: PropTypes.array,
+};
+FormCustomer.propTypes = {
+  fields: PropTypes.arrayOf(PropTypes.object),
+};
 export default FormCustomer;
--- src/components/main-content/content/content-section/customers/TableCustomer.jsx
+++ src/components/main-content/content/content-section/customers/TableCustomer.jsx
@@ -5,17 +5,20 @@ import { Table } from 'react-bootstrap';
 // import components
 
 // import style
+import '../content-section.css';
 
 const TableCustomer = () => {
   return (
-    <>
-      <Table striped bordered hover>
+    <div>
+      <h3>Historique des commandes</h3>
+      <Table className="table,table-responsive" striped bordered hover>
         <thead>
           <tr>
             <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Email</th>
+            <th>Adresse</th>
           </tr>
         </thead>
         <tbody>
@@ -24,21 +27,24 @@ const TableCustomer = () => {
             <td>Mark</td>
             <td>Otto</td>
             <td>@mdo</td>
+            <td>@mdo</td>
           </tr>
           <tr>
             <td>2</td>
             <td>Jacob</td>
             <td>Thornton</td>
             <td>@fat</td>
+            <td>@fat</td>
           </tr>
           <tr>
             <td>3</td>
             <td colSpan="2">Larry the Bird</td>
             <td>@twitter</td>
+            <td>@twitter</td>
           </tr>
         </tbody>
       </Table>
-    </>
+    </div>
   );
 };
 
