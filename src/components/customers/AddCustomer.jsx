de5f56445878a9ead28b17cc53854ed6aa21e2c5
rachOS <g.chamekh@e-nautia.com>
2020-07-08 20:54:19 +0200

    Create from CRUD working

--- src/components/customers/AddCustomer.jsx
+++ src/components/customers/AddCustomer.jsx
@@ -3,33 +3,22 @@ import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import { Form, Container, Col, Row, Button } from 'react-bootstrap';
 
-// import component
-
 // import data
 import fields from './customerFields';
 
 function AddCustomers() {
-  // const initialState = [
-  //   { civility: 'M.', firstname: 'Cham', lastname: 'Gerg' },
-  // ];
   const [customer, setCustomer] = useState([{}]);
   const [inputValue, setInputValue] = useState('');
 
   useEffect(() => {
-    setCustomer([inputValue]);
+    setCustomer(inputValue);
   }, [inputValue]);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const newCustomer = [...customer, inputValue];
-    setCustomer(newCustomer);
-
-    // console.log('NEW', customer);
+  const addCustomer = () => {
     const url = 'http://localhost:5000/api/clients';
-    /* LOOK à partir d'ici Emeline, la ligne 34 fonctionne sans le .then Oo */
-    // Axios.post(url, customer[0]).then((res) => console.log('RES', res.config.data));
-    // Axios.post(url, customer).then((res) => console.log('RES', res[0]));
-    Axios.post(url, customer).then((res) => res[0]);
+    Axios.post(url, customer)
+      .then((res) => res.config)
+      .then((data) => setCustomer(data));
   };
 
   const handleChange = (event) => {
@@ -40,33 +29,13 @@ function AddCustomers() {
     setInputValue(newValue);
   };
 
-  // generate <label/> and <input/>
-  /*  const valueOfFields = Object.values(fields);
-  const createFormClient = valueOfFields.map((field, index) => (
-    <Form.Group>
-    <Form.Label>{field.label}</Form.Label>
-    <Form.Control
-        name={field.name}
-        type={field.type}
-        size="sm"
-        value={inputValue}
-        pattern={field.pattern}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
-    </Form.Group>
-  )); */
-
   // return
   return (
     <Container>
       <Row>
         <Col className="col-form-label-sm">
           <h3 id="">Identité du client</h3>
-          <Form
-            action=""
-            className="form-group"
-            onSubmit={(event) => handleSubmit(event)}
-          >
+          <Form action="" className="form-group" onSubmit={() => addCustomer()}>
             <Form.Group>
               <Form.Label>{fields[0].label}</Form.Label>
               <Form.Control
