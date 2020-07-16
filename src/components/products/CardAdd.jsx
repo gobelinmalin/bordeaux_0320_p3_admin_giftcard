5c4babeebd5aebf12e2b11ea772f2eec8c71ef4f
bz699 <zeebha@gmail.com>
2020-07-16 17:02:42 +0200

    set component + form  AddCard

---
+++ src/components/products/CardAdd.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import '../styles.css';
+import { Form } from 'react-bootstrap';
+
+const cardAdd = () => {
+  return (
+    <div className="formContent">
+      <Form>
+        <p>Ajout de carte cadeaux</p>
+        <Form.Group>
+          <Form.Label>Date d&apos;ajout</Form.Label>
+          <Form.Control> </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Format de carte</Form.Label>
+          <Form.Control> </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Code EAN</Form.Label>
+          <Form.Control> </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Credit</Form.Label>
+          <Form.Control> </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Code de la carte</Form.Label>
+          <Form.Control> </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Code de sécurité</Form.Label>
+          <Form.Control> </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Validité</Form.Label>
+          <Form.Control> </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Multi-utilisation</Form.Label>
+          <Form.Control> </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Statut d&apos;activation</Form.Label>
+          <Form.Control> </Form.Control>
+        </Form.Group>
+      </Form>
+    </div>
+  );
+};
+
+export default cardAdd;
