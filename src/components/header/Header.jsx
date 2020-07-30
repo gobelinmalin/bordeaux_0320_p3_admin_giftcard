28085fbb25b419e6af0514dafc9b3819774bc638
mathildetho <mathilde.thoraninth@gmail.com>
2020-07-30 11:57:08 +0200

    logout done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './header.css';
+import { Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
+import { authContext } from '../../contexts/AuthContext';
 
-function Header() {
+function Header(props) {
+  const { setAuthData, auth } = useContext(authContext);
+  const onLogOut = () => {
+    setAuthData(null);
+    props.history.push('/');
+  };
+
+  const name = auth.data && auth.data.user[0].name;
+
   return (
     <div className="header">
       <div className="title">Console Admin</div>
-      <div className="logout">Logout</div>
+      <div className="logout">
+        <div className="pseudo">{name}</div>
+        <Button variant="primary" type="button" onClick={onLogOut}>
+          Se deconnecter
+        </Button>
+      </div>
     </div>
   );
 }
 
-export default Header;
+Header.propTypes = {
+  history: PropTypes.func,
+};
+
+Header.defaultProps = {
+  history: () => {},
+};
+
+export default withRouter(Header);
